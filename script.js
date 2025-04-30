150f217401007a3a521ea265d603ebcb96825d63
eisoloi <dcfj81632@gmail.com>
2025-04-30 23:39:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const correctPassword = "eisp1";
+const storageKey = "eistabelleData";
+const defaultSorten = [
+  "Schokolade", "Vanille", "Erdbeer", "Himmelblau", "Lila",
+  "Mango", "Menta", "Cookies", "Stracciatella", "Kinderschoko"
+];
+// Rest siehe oben...
