4a49e7eaddb98b07cbd9de5af9d364dc0cd62eba
eisoloi <dcfj81632@gmail.com>
2025-04-30 23:48:22 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,25 @@
-
-const CACHE_NAME = 'eistabelle-cache-v1';
-const URLS_TO_CACHE = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+const CACHE_NAME = "eistabelle-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "style.css",
+  "script.js",
+  "manifest.json",
+  "icons/icon-192.png",
+  "icons/icon-512.png"
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE)));
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request, { ignoreSearch: true }).then(response => response || fetch(event.request)));
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
