f5bb587e093364be3075137addcf196e199a8df6
eisoloi <dcfj81632@gmail.com>
2025-04-30 23:51:08 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = "eistabelle-cache-v1";
 const urlsToCache = [
-  "index.html",
-  "style.css",
-  "script.js",
-  "manifest.json",
-  "icons/icon-192.png",
-  "icons/icon-512.png"
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
