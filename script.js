799f27821a43b7d079ef580715e3a63e2ae66578
eisoloi <dcfj81632@gmail.com>
2025-05-05 21:12:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -35,15 +35,27 @@ function startApp() {
 }
 
 // Startbildschirm bei erstem Besuch anzeigen
-window.addEventListener("DOMContentLoaded", () => {
-  const visited = localStorage.getItem(startScreenKey);
-  if (visited) {
-    document.getElementById("startScreen").style.display = "none";
-    document.getElementById("app").style.display = "block";
-    initializeData();
-  } else {
-    document.getElementById("startScreen").style.display = "block";
-    document.getElementById("app").style.display = "none";
+window.addEventListener('beforeinstallprompt', (e) => {
+  console.log("beforeinstallprompt fired");
+  e.preventDefault();
+  deferredPrompt = e;
+
+  const installBtn = document.getElementById('installButton');
+  if (installBtn && !localStorage.getItem('homescreenShown')) {
+    installBtn.style.display = 'inline-block';
+    installBtn.addEventListener('click', () => {
+      installBtn.style.display = 'none';
+      deferredPrompt.prompt();
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('App wurde installiert.');
+        } else {
+          console.log('App-Installation abgelehnt.');
+        }
+        deferredPrompt = null;
+        localStorage.setItem('homescreenShown', 'true');
+      });
+    });
   }
 });
 
